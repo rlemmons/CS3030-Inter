2f59e9f5bc3d120e5595b9c0073f0f8df66e3e50
Rob Lemmons <robertlemmons@mail.weber.edu>
2017-03-28 13:32:19 -0600

    init

---
+++ task1_mod1.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+import sys
+"""
+This is Module 1, task 1
+"""
+
+
+# Main function
+def main():
+    """
+    Test Function
+    """
+    pass
+
+
+
+if __name__ == "__main__":
+    # Call Main
+    main()
+
+    exit(0)
