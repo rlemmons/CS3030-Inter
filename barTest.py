6ad8f0f224ea767fa7a05c639ac686a05ce31e9e
David Mariluch <davidmariluch@mail.weber.edu>
2017-03-30 03:06:33 +0000

    added template for barTest.py

---
+++ barTest.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+import sys
+#import mod3.py
+
+def get_url_reader():
+    """
+    Function gets file from url and returns a reader object
+    """
+
+# Main function
+def main():
+    """
+    For each line in reader object returned from get_url_reader()
+        print the digit with imported mod3.py function printDigit(d)
+        print the bar code with imported mod3.py function printBarCode(zipCode)
+    """
+    # for line in get_url_reader():
+        #mod3.printDigit(d)
+        #mod3.printBarCode(zipCode)
+    
+
+if __name__ == "__main__":
+    # Call Main
+    main()
+
+    exit(0)
