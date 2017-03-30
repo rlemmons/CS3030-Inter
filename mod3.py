fb7f8335813b7fe03139231b7b1b53c2df27afac
Rob Lemmons <robertlemmons@mail.weber.edu>
2017-03-29 21:07:27 -0600

    init commit

---
+++ mod3.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+import sys
+"""
+This file converts a postal zip code 
+into a bar code, with a check at the end.
+the check takes the sum of the zip code 
+and brings the total to the next multiple 
+of ten.
+"""
+
+
+def printDigit(d):
+    """
+
+    """
+    pass
+
+def printBarCode(zipCode):
+    """
+
+    """
+    pass
+
+
+# Main function
+def main():
+    """
+    Test Function
+    """
+    pass
+
+
+
+if __name__ == "__main__":
+    # Call Main
+    main()
+
+    exit(0)
